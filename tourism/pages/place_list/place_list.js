21c98bc67046a1e4dad1b0e62eb44c9a0c9521b6
hwj <807109746@qq.com>
2018-12-30 16:23:58 +0800

    登录界面、地点列表

---
+++ tourism/pages/place_list/place_list.js
@@ -0,0 +1,74 @@
+// pages/place_list/place_list.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    trips:[{
+      id:1,
+      name:'夫子庙',
+      cover_image_w640: "http://photos.breadtrip.com/photo_2017_07_08_646eb65bf7e95d734467fa6c8e36da03.jpg?imageView/1/w/640/h/480/q/85"
+    },{
+        id: 2,
+        name: '中山陵',
+        cover_image_w640: "http://photos.breadtrip.com/photo_2017_10_26_862ba742c7ef598283234de7bf155e50.jpg?imageView/1/w/640/h/480/q/85"
+    }]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
